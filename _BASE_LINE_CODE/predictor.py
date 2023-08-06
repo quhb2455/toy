774aa28105561c06cd71eb387710e0a3bba7134e
DESKTOP-MHP82SA\quhb2 <quhb2455@gmail.com>
2023-08-06 15:16:17 +0900

    baseline

---
+++ _BASE_LINE_CODE/predictor.py
@@ -0,0 +1,54 @@
+
+from utils import score, label_dec
+
+import torch
+import numpy as np
+import os
+from tqdm import tqdm
+from glob import glob
+
+import pandas as pd
+
+class Predictor() :
+    def __init__(self) -> None:
+        pass
+        
+    def prediction(self, **cfg) :
+        self.pred_weight_load(cfg["weight_path"])
+        self.model.eval()        
+        model_preds = []
+        with torch.no_grad() :
+            for img in tqdm(self.test_loader) :
+                model_preds += self.predict_on_batch(img, **cfg)
+        
+        self.save_to_csv(model_preds, **cfg)
+        
+    
+    def predict_on_batch(self, img, **cfg) :
+        img = img.to(cfg["device"])
+        return self.model(img).argmax(1).detach().cpu().numpy().tolist()
+        
+    def save_to_csv(self, results, **cfg) :
+        _label_dec = label_dec(cfg["label_name"])
+
+        img_name_list = ["TEST_"+os.path.splitext(os.path.basename(p))[0] for p in glob(os.path.join(cfg["data_path"], "*"))]
+        res_label_list = [_label_dec[i] for i in results]        
+        
+        df = pd.DataFrame({"id" : img_name_list, "label":res_label_list})
+        df.to_csv(os.path.join(cfg["output_path"],os.path.splitext(cfg["weight_path"].split("/")[-1])[0])+".csv", 
+                  index=False)
+    
+    def pred_weight_load(self, weight_path) :
+        checkpoint = torch.load(weight_path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+    
+    # def pred_weight_load(self, weight_path) :
+    #     checkpoint = torch.load(weight_path)
+    #     # self.model.load_state_dict(checkpoint['model_state_dict'])
+    #     self.base_model.load_state_dict(checkpoint["base_model_state_dict"]),
+    #     self.head_1.load_state_dict(checkpoint["head_1_state_dict"]),
+    #     self.head_2.load_state_dict(checkpoint["head_2_state_dict"]),
+    #     self.head_3.load_state_dict(checkpoint["head_3_state_dict"]),
+    #     self.head_4.load_state_dict(checkpoint["head_4_state_dict"]),
+    #     self.head_5.load_state_dict(checkpoint["head_5_state_dict"]),
+    #     self.head_6.load_state_dict(checkpoint["head_6_state_dict"]),
---
+++ _BASE_LINE_CODE/trainer.py
@@ -0,0 +1,130 @@
+from utils import score, logging
+
+import torch
+import numpy as np
+import os
+from tqdm import tqdm
+
+class Trainer() :
+    def __init__(self) -> None:
+        # self.train_loader = None
+        # self.valid_loader = None
+        # self.criterion = None
+        # self.optimizer = None
+        # self.model = None
+        
+        self.best_score = 0
+        self.early_stop_cnt = 0
+        
+    def run(self, **cfg) :
+        self.log = logging(cfg["log_path"])
+        
+        start_epoch = self.train_weight_load(cfg["weight_path"]) if cfg["reuse"] else 0
+            
+        for e in range(start_epoch, cfg["epochs"]) :
+            self.train_on_epoch(e, **cfg)
+            valid_acc, valid_loss = self.valid_on_epoch(e, **cfg)
+            
+            self.logging({"Valid ACC" : valid_acc, "Valid Loss" : valid_loss}, e)
+            self.save_checkpoint(e, valid_acc, **cfg)
+            
+            if self.early_stop_cnt == cfg["early_stop_patient"] :
+                print("=== EARLY STOP ===")
+                break
+    
+    def train_weight_load(self, weight_path) :
+        checkpoint = torch.load(weight_path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])        
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        return checkpoint['epoch'] + 1
+        
+    def train_on_epoch(self, epoch, **cfg):
+        self.model.train()
+        train_acc, train_loss = [], []
+        tqdm_train = tqdm(self.train_loader)
+        for step, (img, label) in enumerate(tqdm_train) :
+            batch_res = self.train_on_batch(img, label, **cfg)
+            
+            train_acc.append(batch_res["acc"])
+            train_loss.append(batch_res["loss"])
+            
+            log = {
+                "Epoch" : epoch,
+                "Training Acc" : np.mean(train_acc),
+                "Training Loss" : np.mean(train_loss),
+            }
+            tqdm_train.set_postfix(log)
+            self.logging(log, step)
+            
+        # self.scheduler.step()
+        
+    def train_on_batch(self, img, label, **cfg) :
+        self.optimizer.zero_grad()
+
+        img = img.to(cfg["device"])
+        label = label.to(cfg["device"])
+        
+        output = self.model(img)
+        loss = self.criterion(output, label)
+        
+        loss.backward()
+        self.optimizer.step()
+        
+        acc = score(label, output)
+        
+        batch_metric = {
+            "acc" : acc,
+            "loss" : loss.item()
+        }
+        return batch_metric 
+    
+    def valid_on_epoch(self, epoch, **cfg):
+        self.model.eval()
+        valid_acc, valid_loss = [], []
+        tqdm_valid = tqdm(self.valid_loader)
+        for step, (img, label) in enumerate(tqdm_valid) :
+            batch_res = self.valid_on_batch(img, label, **cfg)
+            
+            valid_acc.append(batch_res["acc"])
+            valid_loss.append(batch_res["loss"])
+            log = {
+                "Epoch" : epoch,
+                "Validation Acc" : np.mean(valid_acc),
+                "Validation Loss" : np.mean(valid_loss),
+            }
+            tqdm_valid.set_postfix(log)
+            
+        return np.mean(valid_acc), np.mean(valid_loss)
+    
+    def valid_on_batch(self, img, label, **cfg):
+        img = img.to(cfg["device"])
+        label = label.to(cfg["device"])
+        
+        output = self.model(img)
+        loss = self.criterion(output, label)
+        
+        acc = score(label, output)
+        
+        batch_metric = {
+            "acc" : acc,
+            "loss" : loss.item()
+        }
+        
+        return batch_metric
+    
+    def save_checkpoint(self, epoch, val_acc, **cfg) :
+        if self.best_score < val_acc:
+            self.best_score = val_acc
+            torch.save({
+                "epoch": epoch,
+                "model_state_dict": self.model.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict()
+            }, os.path.join(cfg["save_path"], str(epoch) + 'E-val' + str(self.best_score) + '-' + cfg["model_name"] + '.pth'))
+            self.early_stop_cnt = 0 
+        else : 
+            self.early_stop_cnt += 1
+            
+    def logging(self, log_dict, step) :
+        for k, v in log_dict.items():
+            if k == "Epoch" : continue
+            self.log.add_scalar(k, v, step)
